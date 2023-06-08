f6aabde1c1eb9b1cf18c46eb8ecbe3e29ddb18be
thexamlomaximal@gmail.com <thexamlomaximal@gmail.com>
2023-06-08 19:55:46 +0300

    fix checkable state button

--- common/ui/QuestionTorsonWidget.cpp
+++ common/ui/QuestionTorsonWidget.cpp
@@ -8,6 +8,11 @@ QuestionTorsonWidget::QuestionTorsonWidget(QWidget *parent) : QWidget(parent), u
 
 void QuestionTorsonWidget::onButtonAnswerClicked() {
 
+    this->uiQuestionsTorston->button1->setChecked(false);
+    this->uiQuestionsTorston->button2->setChecked(false);
+    this->uiQuestionsTorston->button3->setChecked(false);
+    this->uiQuestionsTorston->button4->setChecked(false);
+
     emit signalAnswered(this->uiQuestionsTorston->button1->isChecked(),
                         this->uiQuestionsTorston->button2->isChecked(),
                         this->uiQuestionsTorston->button3->isChecked(),
